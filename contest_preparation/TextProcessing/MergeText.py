d8963a12b1bcbf42b1ce8fa74fb611d62a05fbae
bosl95 <34594339+bosl95@users.noreply.github.com>
2020-08-28 10:45:49 +0900

    Update MergeText.py

--- contest_preparation/TextProcessing/MergeText.py
+++ contest_preparation/TextProcessing/MergeText.py
@@ -6,10 +6,17 @@ import os, shutil
 CList = os.listdir('cross')
 TList = os.listdir('traffic')
 
-for c in CList:
+CL = CList.copy()
+for c in CL:
     if c in TList:
+        TList.remove(c)
+        CList.remove(c)
+
         with open('cross/'+c, 'r') as cf, open('traffic/'+c, 'r') as tf:
             f = open('new/'+c, 'w')
             f.write(''.join(cf.readlines()+tf.readlines()))
-    else:   # 만약에 신호등 라벨링은 있는데 횡단보도 라벨링이 없으면 그냥 신호등 라벨링 복사
-        shutil.copy('traffic/'+c, 'new/'+c)
+
+for t in TList:
+    shutil.copy('traffic/' +t, 'new/' + t)
+for c in CList:
+    shutil.copy('cross/' + c, 'new/' + c)
