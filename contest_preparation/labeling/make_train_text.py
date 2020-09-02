8bc3c2bc692d5d30654ad7d11c620ad4b75a0c76
sor <bosl95@naver.com>
2020-09-03 00:27:06 +0900

    make_train_text : 최종 train.txt make file

--- contest_preparation/labeling/make_train_text.py
+++ contest_preparation/labeling/make_train_text.py
@@ -9,3 +9,14 @@ if __name__ == '__main__':
             for f in glob.glob("dataset/"+dataFolder+"/"+f+"/*.jpg"):
                 print("data/obj/"+f.replace('\\', '/'))
                 text.write("data/obj/"+f.replace('\\', '/')+'\n')
+
+
+    # ver 2
+    # folderList = os.listdir('dataset')
+    # for dataFolder in folderList:
+    #     text = open('all_train.txt', 'a')
+    #     for file in glob.glob("dataset/"+dataFolder+"/*.jpg"):
+    #         print("data/obj/"+dataFolder+"/"+file.split('\\')[1])
+    #         text.write("data/obj/"+dataFolder+"/"+file.split('\\')[1]+'\n')
+
+
